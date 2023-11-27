71706c741162adbc0f15eff0c6b3dd50678ea91d
HughCoder <hughz926@gmail.com>
2023-11-27 22:36:09 +0800

    today

--- note/NoteDemo.java
+++ note/NoteDemo.java
@@ -3,6 +3,6 @@ package note;
 public class NoteDemo {
     public  static void main(String[] args){
         //以下是一个打印语句
-        System.out.println("我开始学习java，好开心");
+        System.out.println("我开始学习java，好");
     }
 }
